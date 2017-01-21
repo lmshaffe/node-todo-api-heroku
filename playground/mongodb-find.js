15bedfdf00606372d38e5bd8aae5e62561616cbd
Lee Shaffer <shaffuh@gmail.com>
2017-01-21 13:15:51 -0500

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+const {MongoClient, ObjectID} = require ('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) return console.log('Unable to connect to MongoDB server');
+
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({_id:ObjectID('588388b6d40f3bca5eab3915')}).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos');
+  // })
+
+  // db.collection('Todos').find({}).count().then((count) => {
+  //   console.log('Todos');
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos');
+  // })
+
+  db.collection('Users').find({name: 'Lee'}).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos');
+  })
+
+  // db.close();
+});
