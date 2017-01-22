4bbbc2968ca64564012ab01140015f8cee8785b2
Lee Shaffer <shaffuh@gmail.com>
2017-01-22 13:49:01 -0500

    Add tests for POST /users/login

--- server/tests/server.tests.js
+++ server/tests/server.tests.js
@@ -198,7 +198,7 @@ describe('POST /users', () => {
         expect(user).toExist()
         expect(user.password).toNotBe(password)
         done()
-      })
+      }).catch((e) => done(e))
     })
   })
 
@@ -235,5 +235,73 @@ describe('POST /users', () => {
     .send({email: duplicateEmail, password})
     .expect(400)
     .end(done)
+  })
+})
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[1].email,
+      password: users[1].password
+    })
+    .expect(200)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toExist()
+    })
+    .end((err, res) => {
+      if (err) done(err)
+
+      User.findById(users[1]._id).then((user) => {
+        expect(user.tokens[0]).toInclude({
+          access: 'auth',
+          token: res.headers['x-auth']
+        })
+        done()
+      }).catch((e) => done(e))
+    })
+  })
+
+  it('should reject invalid email', (done) => {
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[1].email + 'bad',
+      password: users[1].password
+    })
+    .expect(400)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toNotExist()
+    })
+    .end((err, res) => {
+      if (err) done(err)
+
+      User.findById(users[1]._id).then((user) => {
+        expect(user.tokens.length).toBe(0)
+        done()
+      }).catch((e) => done(e))
+    })
+  })
+
+  it('should reject invalid password', (done) => {
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[1].email,
+      password: 'invalidPassword'
+    })
+    .expect(400)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toNotExist()
+    })
+    .end((err, res) => {
+      if (err) done(err)
+
+      User.findById(users[1]._id).then((user) => {
+        expect(user.tokens.length).toBe(0)
+        done()
+      }).catch((e) => done(e))
+    })
   })
 })
