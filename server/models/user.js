d1f3031045f340e85cf9de3b4f8538f50aa630f6
Lee Shaffer <shaffuh@gmail.com>
2017-01-22 14:08:09 -0500

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,16 @@ UserSchema.methods.generateAuthToken = function () {
   })
 }
 
+UserSchema.methods.removeToken = function(token) {
+  var user = this
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  })
+}
+
 UserSchema.statics.findByToken = function (token) {
   var User = this
   var decoded;
@@ -86,9 +96,6 @@ UserSchema.statics.findByCredentials = function (email, password) {
       })
     })
   })
-
-
-
 }
 
 UserSchema.pre('save', function(next) {
